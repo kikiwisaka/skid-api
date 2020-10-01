8a94c4edd37da4e175ea9dfa33113ceb713d4cdd
kikiwisaka <rusmawi@gmail.com>
2020-10-02 04:12:19 +0700

    add heroku to package

--- routes/donations.js
+++ routes/donations.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const auth = require('../middleware/auth');
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
 const {
     check,
     validationResult
